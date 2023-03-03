8f12dc20559573b23d4f5109585f6fed72115225
DreamHollow <404errorman@gmail.com>
2023-03-03 00:59:22 -0500

    Main.cpp: Comments added

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include "Application.hpp"
 #include "CMakeEasyConfig.h"
 
+// Needs C style arguments for Versioning to work.
 int main(int argc, char* argv[])
 {
     // Heap allocated both classes to prevent overflow of stack.
