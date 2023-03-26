5a782ea37346bd02022cee28fcdd62b20405ef30
DreamHollow <404errorman@gmail.com>
2023-03-26 11:44:15 -0400

    install.sh: Added

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Building Release version..."
+
+mkdir Release
+cd Release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make install
+
+echo "Release build finished."
+echo "Do not build CMakeEasy. Binary already installed to system."
