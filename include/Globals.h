a41a12155433705a344803a284f0c7327a1a7425
DreamHollow <404errorman@gmail.com>
2023-07-06 10:30:16 -0400

    Globals.h: Added 'OS' protocols

--- include/Globals.h
+++ include/Globals.h
@@ -1,5 +1,26 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
+
+#ifdef __unix__ // Linux
+#define OS_WINDOWS 0
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+static struct passwd *pw = getpwuid(getuid());
+static const char *homedir = pw->pw_dir;
+#endif
+
+#ifdef _WIN32
+#define OS_WINDOWS 1
+#include <windows.h>
+#endif
+
+#ifdef _WIN64
+#define OS_WINDOWS 1
+#include <windows.h>
+#endif
+
 #include <string>
 
 static bool debugging = false;
