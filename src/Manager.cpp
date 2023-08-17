5d2c4efdf9504ae2593ef2b283f7d83724e9076a
DreamHollow <404errorman@gmail.com>
2023-08-17 16:25:32 -0400

    Manager.cpp: Altered file init

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -2,12 +2,46 @@
 
 Manager::Manager()
 {
-  this->init_vars();
-  this->init_file();
+  //this->init_vars();
+  //this->init_file();
+};
+
+Manager::Manager(std::string target_file, bool read_only)
+{
+  this->file_name = target_file;
+  this->read_only = read_only;
+
+  std::string f_slash = "/";
+
+  if(!this->read_only)
+  {
+    if(OS_WINDOWS)
+    {
+      //std::string list_dir = "Lists";
+
+      system("mkdir Lists");
+      homedir = "Lists/CMakeLists.txt";
+      file_dir = file_name;
+    }
+    else // Linux
+    {
+      file_name = homedir + f_slash + file_name;
+    }
+  }
+
+  std::cout << db_msg("\n");
+  std::cout << db_msg("File name for instance: ");
+  std::cout << db_msg(file_name);
+  std::cout << db_msg("\n");
+
+  // Initialize file with same settings
+  init_file();
 };
 
 Manager::~Manager()
 {
+  std::cout << db_msg("Manager deconstructed...\n");
+
   free();
 };
 
@@ -15,113 +49,83 @@ Manager::~Manager()
 /// Outputs for debugger.
 void Manager::free()
 {
-  this->file.close();
+  if(infile.is_open())
+  {
+    this->infile.close();
 
-  std::cout << db_msg("Manager closed the open file.\n");
+    std::cout << db_msg("Manager closed a read-only file.\n");
+  }
+
+  if(outfile.is_open())
+  {
+    this->outfile.close();
+
+    std::cout << db_msg("Manager closed an output file.\n");
+  }
 }
-
-void Manager::init_vars()
-{
-  this->yes_no = 0;
-};
 
 /// @brief The file is initialized early into the program, along with the directory.
 void Manager::init_file()
 {
-  std::string f_slash = "/";
-
-  if(!OS_WINDOWS) // Linux
+  // Initialize an AltString file
+  if(this->read_only == true)
   {
-    // This should be the working $HOME directory
-
-    file_name = homedir + f_slash + file_name; // Linux Home
-
-    file_dir = file_name; // ~/home
+    std::cout << db_msg("\n");
+    std::cout << db_msg("Read-only function currently undefined.\n");
+    std::cout << db_msg("\n");
   }
   else
   {
-    // There are few easier ways to do this on Windows
-    system("mkdir Lists");
-    homedir = "Lists/CMakeLists.txt";
-    file_name = homedir;
+    std::cout << db_msg("\n");
+    std::cout << db_msg("Read-only flag set to false, initializing CMakeLists.txt...\n");
+    std::cout << db_msg("\n");
 
-    file_dir = file_name; // Program Files
-  }
-
-  // This catch-all should really be left in for worst-case file writing scenarios.
-  // If the CMakeLists.txt file cannot be accessed, it should not be written to!
-
-  if(!this->file.is_open())
-  {
     try
     {
-      std::cout << db_msg("\n");
-      std::cout << db_msg("Warning: If CMakeLists.txt exists in /src folder,\n");
-      std::cout << db_msg("it will be overwritten!\n");
-
-      if(OS_WINDOWS)
+      if(!outfile.is_open())
       {
-        std::cout << "\n";
-        std::cout << "Windows system detected." << "\n";
-        std::cout << "\n";
-        std::cout << "CMakeEasy will automatically write your CMakeLists.txt\n";
-        std::cout << " to a sub-folder called 'Lists'." << "\n";
-      }
-      else
-      {
-        std::cout << "\n";
-        std::cout << "Non-Windows system detected. Linux system assumed." << "\n";
-        std::cout << "\n";
-        std::cout << "CMakeEasy will automatically write your CMakeLists.txt\n";
-        std::cout << "to your home directory folder." << "\n";
-      }
+        std::cout << db_msg("\n");
+        std::cout << db_msg("Current file path: ");
+        std::cout << db_msg(file_name);
+        std::cout << db_msg("\n");
+        std::cout << db_msg("Attempting to create/edit CMakeLists.txt...\n");
 
-      std::cout << "\n";
-      std::cout << "Please remove any CMakeList.txt files that you" << "\n";
-      std::cout << "Do not wish to lose." << "\n";
-      std::cout << "\n";
-      std::cout << "------" << "\n";
-      std::cout << "\n";
-      std::cout << "Initializing file settings..." << "\n";
+        outfile.open(file_name.c_str(), std::ios::out | std::ios::trunc);
 
-      std::cout << db_msg("Current file path:\n");
-      std::cout << db_msg(file_name);
-      std::cout << db_msg("\n");
-      std::cout << db_msg("Program attempting to open file 'CMakeLists'...\n");
+        if(outfile.fail())
+        {
+          std::cout << "ERROR: CMakeLists.txt file is unable to be opened.\n";
+          std::cout << "CMakeEasy cannot continue.\n";
 
-      file.open(file_name.c_str(), std::ios::out | std::ios::trunc);
+          free();
 
-      if(file.fail())
-      {
-        std::cout << "The CMakeLists.txt file could not be edited." << "\n";
-        std::cout << "An error occurred: 'Unusuable file path'." << "\n";
+          throw "Unusuable file path.";
+        }
+        else // File is good
+        {
+          std::cout << "CMakeLists.txt file is valid, allowing edits.\n";
 
-        free();
-        
-        throw "Bad CMakeLists directory argument.";
+          std::cout << db_msg("\n");
+          std::cout << db_msg(file_name);
+          std::cout << db_msg("\n");
+          std::cout << db_msg("is ready to be modified.\n");
+        }
       }
     }
     catch(const std::exception& e)
     {
       std::cerr << e.what() << '\n';
     }
-
-    if(file.good())
-    {
-      std::cout << db_msg("CMakeLists.txt is good, allowing edits.\n");
-
-      std::cout << "\n";
-      std::cout << file_name << " is ready to be modified." << "\n";
-      std::cout << "\n";
-    }
   }
+  // This catch-all should really be left in for worst-case file writing scenarios.
+  // If the CMakeLists.txt file cannot be accessed, it should not be written to!
 };
 
 /// @brief Writes char arrays and strings into files.
 /// @param context 
 void Manager::write(std::string context)
 {
-  file << context;
+  outfile << context;
 };
 
 /// @brief Converts numerical values into strings, then into files.
@@ -130,5 +134,5 @@ void Manager::write(int num)
 {
   std::string s = std::to_string(num);
 
-  file << s;
+  outfile << s;
 };
