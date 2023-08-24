f8191d791e02440ba421708ecf3c006cbde11fde
DreamHollow <404errorman@gmail.com>
2023-08-24 19:06:03 -0400

    Globals.h: Debugging state set to true

--- include/Globals.h
+++ include/Globals.h
@@ -23,7 +23,7 @@ static const char *homedir = pw->pw_dir;
 
 #include <string>
 
-static bool debug = false;
+static bool debug = true;
 static char db_string[]{"DEBUG: "};
 static short yes_no = 0;
 
