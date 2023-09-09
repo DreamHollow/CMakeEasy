6da047117384906261d8854dd224e73cf06b9f03
DreamHollow <404errorman@gmail.com>
2023-09-09 02:54:47 -0400

    winbuild.ps1: Removed broken script

--- winbuild.ps1
+++
@@ -1,6 +0,0 @@
-# Powershell Script that builds CMakeEasy (Debug)
-# Powershell has built in security features to prevent a full installation
-# To override this and install CMakeEasy on your system, you will have to refer to the installation
-# instructions on the GitHub repo
-
-cmake -S . -B build
