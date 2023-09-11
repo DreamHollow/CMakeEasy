2c49e13d7eea1ee732e61dce570048e04736c6ea
DreamHollow <404errorman@gmail.com>
2023-09-11 19:37:52 -0400

    install.sh: Temporarily disabled

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # Only works on Linux version!
 # You must run as SU for this to actually install!
 
-echo "Building Release version..."
+#echo "Building Release version..."
 
-mkdir Release
-cd Release
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make install
+#mkdir Release
+#cd Release
+#cmake -DCMAKE_BUILD_TYPE=Release ..
+#make install
 
-echo "Release build finished."
-echo "Do not build CMakeEasy. The binary should be installed to the system."
+#echo "Release build finished."
+#echo "Do not build CMakeEasy. The binary should be installed to the system."
