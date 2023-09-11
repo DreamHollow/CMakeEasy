6fd766c4d2401613b68cc875c3754b9f252bc321
DreamHollow <404errorman@gmail.com>
2023-09-11 14:43:05 -0400

    Manager.cpp: File handling changes

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -2,8 +2,7 @@
 
 Manager::Manager()
 {
-  //this->init_vars();
-  //this->init_file();
+
 };
 
 Manager::Manager(std::string target_file, bool read_only)
@@ -17,8 +16,6 @@ Manager::Manager(std::string target_file, bool read_only)
   {
     if(OS_WINDOWS)
     {
-      //std::string list_dir = "Lists";
-
       system("mkdir Lists");
       homedir = "Lists/CMakeLists.txt";
       file_dir = file_name;
@@ -34,14 +31,52 @@ Manager::Manager(std::string target_file, bool read_only)
   std::cout << db_msg(file_name);
   std::cout << db_msg("\n");
 
-  // Initialize file with same settings
-  if(file_initialized(read_only))
+  try
   {
-    std::cout << file_name + " initialized with no problems.\n";
+    if(read_only == true)
+    {
+      std::cout << db_msg("Read-only function currently undefined.\n");
+    }
+    else // CMakeLists
+    {
+      std::cout << db_msg("Read-only flag set to false, initializing CMakeLists.txt...\n");
+
+      if(!outfile.is_open())
+      {
+        std::cout << db_msg("\n");
+        std::cout << db_msg("Current file path: ");
+        std::cout << db_msg(file_name);
+        std::cout << db_msg("\n");
+        std::cout << db_msg("Attempting to create/edit CMakeLists.txt...\n");
+
+        outfile.open(file_name.c_str(), std::ios::out | std::ios::trunc);
+
+        // Flags a boolean fail -- moves program to application, then main
+        if(outfile.fail())
+        {
+          std::cout << "ERROR: CMakeLists.txt file is unable to be opened.\n";
+          std::cout << "CMakeEasy cannot continue.\n";
+          std::cout << "\n";
+
+          free_data();
+
+          throw "Unable to open file.";
+        }
+        else // File is good
+        {
+          std::cout << "CMakeLists.txt file is valid, allowing edits.\n";
+
+          std::cout << db_msg("\n");
+          std::cout << db_msg(file_name);
+          std::cout << db_msg("\n");
+          std::cout << db_msg("is ready to be modified.\n");
+        }
+      }
+    }
   }
-  else
+  catch(const std::exception& e)
   {
-    std::cout << file_name + " failed to initialize.\n";
+    std::cerr << e.what() << '\n';
   }
 };
 
@@ -49,12 +84,12 @@ Manager::~Manager()
 {
   std::cout << db_msg("Manager deconstructed...\n");
 
-  free();
+  free_data();
 };
 
 /// @brief Forces file to close to prevent leaks.
 /// Outputs for debugger.
-void Manager::free()
+void Manager::free_data()
 {
   if(infile.is_open())
   {
@@ -78,53 +113,6 @@ void Manager::free()
     std::cout << db_msg("Manager wanted to close an output file, there was nothing.\n");
   }
 }
-
-/// @brief The file is initialized early into the program, along with the directory.
-bool Manager::file_initialized(bool &ro)
-{
-  if(ro == true) // AltString
-  {
-    std::cout << db_msg("Read-only function currently undefined.\n");
-  }
-  else // CMakeLists
-  {
-    std::cout << db_msg("Read-only flag set to false, initializing CMakeLists.txt...\n");
-
-    if(!outfile.is_open())
-    {
-      std::cout << db_msg("\n");
-      std::cout << db_msg("Current file path: ");
-      std::cout << db_msg(file_name);
-      std::cout << db_msg("\n");
-      std::cout << db_msg("Attempting to create/edit CMakeLists.txt...\n");
-
-      outfile.open(file_name.c_str(), std::ios::out | std::ios::trunc);
-
-      // Flags a boolean fail -- moves program to application, then main
-      if(outfile.fail())
-      {
-        std::cout << "ERROR: CMakeLists.txt file is unable to be opened.\n";
-        std::cout << "CMakeEasy cannot continue.\n";
-        std::cout << "\n";
-
-        free();
-
-        return false;
-      }
-      else // File is good
-      {
-        std::cout << "CMakeLists.txt file is valid, allowing edits.\n";
-
-        std::cout << db_msg("\n");
-        std::cout << db_msg(file_name);
-        std::cout << db_msg("\n");
-        std::cout << db_msg("is ready to be modified.\n");
-      }
-    }
-  }
-
-  return true;
-};
 
 /// @brief Writes char arrays and strings into files.
 /// @param context 
