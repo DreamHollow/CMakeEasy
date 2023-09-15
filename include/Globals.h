d945c64db67935cdddb9d54f69b740ff3c98f516
DreamHollow <404errorman@gmail.com>
2023-09-14 22:52:34 -0400

    Globals.h: Fixed definition errors

--- include/Globals.h
+++ include/Globals.h
@@ -1,7 +1,12 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#ifdef __unix__ // Linux
+#ifdef _WIN32
+#define OS_WINDOWS 1
+#include <windows.h>
+#endif
+
+#ifdef linux // Linux
 #define OS_WINDOWS 0
 #include <stdio.h>
 #include <unistd.h>
@@ -9,11 +14,6 @@
 #include <pwd.h>
 static struct passwd *pw = getpwuid(getuid());
 static const char *homedir = pw->pw_dir;
-#endif
-
-#ifdef _WIN32
-#define OS_WINDOWS 1
-#include <windows.h>
 #endif
 
 #ifdef _WIN64
