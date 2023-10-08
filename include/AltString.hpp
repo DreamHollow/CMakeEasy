2801290df61dde0c5cbc2d7b228a4c197ff1cae6
DreamHollow <404errorman@gmail.com>
2023-10-08 14:37:18 -0400

    AltString.hpp: Added enum

--- include/AltString.hpp
+++ include/AltString.hpp
@@ -2,6 +2,11 @@
 #define ALTSTRING_HPP
 #include "Manager.hpp"
 
+enum DECLARATION { CMAKE_MIN = 0, PROJ, VERS_NUM, PROGRAM_LANG, CONF_FILE,
+    TARGET_LINK_LIB, CMAKE_STANDARD, SET_DATA, ADD_EXEC, INCLUDE_DIR,
+    TARGET_COMPILE, PROJECT_NAME, PUB, PRIV, INTERFACE, CMAKE_REQ,
+    REQ, COMP, FIND_PACK, VERBOSE_MAKE };
+
 /// @brief Reduces some output clutter and loosely handles inputs.
 class AltString
 {
